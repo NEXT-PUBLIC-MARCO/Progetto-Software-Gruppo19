8c6946e0a655ad39db5fb659312d1be76f29ae33
mariowsop <m.soppelsa@studenti.unisa.it>
2026-06-01 20:37:20 +0200

    implementazione player sequenziale per playlist

--- src/main/java/it/gruppo19/progetto_music_player/model/SequentialPlaylistIterator.java
+++ src/main/java/it/gruppo19/progetto_music_player/model/SequentialPlaylistIterator.java
@@ -1,7 +1,55 @@
 package it.gruppo19.progetto_music_player.model;
 
-public class SequentialPlaylistIterator {
+public class SequentialPlaylistIterator implements PlaylistIterator {
+    private final PlaylistModel p;
+    private int currentIndex;
 
+    public SequentialPlaylistIterator(PlaylistModel p, int currentIndex) {
+        this.p = p;
+        if(p==null || currentIndex < -1 || currentIndex >= p.size()){
+            this.currentIndex = -1;
+        }
+        else{
+            this.currentIndex = currentIndex;
+        }
+    }
 
+    @Override
+    public boolean hasNext(){
+        return p != null && currentIndex+1 <= p.size();
+    }
 
+    @Override
+    public  BranoModel next(){
+        if(!hasNext()){
+            return null;
+        }
+        return p.getBrano(currentIndex++);
+    }
+
+    @Override
+    public boolean hasPrevious(){
+        return p != null && currentIndex-1 >= 0;
+    }
+
+    @Override
+    public  BranoModel previous(){
+         if(!hasPrevious()){
+        return null;
+        }
+        return p.getBrano(currentIndex--);
+    }
+
+    @Override
+    public BranoModel current(){
+        if(currentIndex<0 || currentIndex >= p.size() || p == null){
+            return null;
+        }
+        return p.getBrano(currentIndex);
+    }
+
+    @Override
+    public void reset(){
+        currentIndex = -1;
+    }
 }
--- src/main/java/it/gruppo19/progetto_music_player/model/playback/SequentialPlaylistStrategy.java
+++ src/main/java/it/gruppo19/progetto_music_player/model/playback/SequentialPlaylistStrategy.java
@@ -4,7 +4,7 @@ import it.gruppo19.progetto_music_player.model.PlaylistIterator;
 import it.gruppo19.progetto_music_player.model.PlaylistModel;
 import it.gruppo19.progetto_music_player.model.SequentialPlaylistIterator;
 
-public class SeqentialPlaylistStrategy implements PlaybackOrderStrategy{
+public class SequentialPlaylistStrategy implements PlaybackOrderStrategy{
     @Override
     public PlaylistIterator createIterator(PlaylistModel p){
         return new SequentialPlaylistIterator(p);
