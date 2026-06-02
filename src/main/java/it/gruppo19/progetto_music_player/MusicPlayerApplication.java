601c9b40670106393d47f01f24e85b4f6266e51d
Katseisu <c.mirante1@studenti.unisa.it>
2026-06-02 12:21:15 +0200

    Change of Entry Point
    Spostato l'entry point dell'applicazione al RealMainController

--- src/main/java/it/gruppo19/progetto_music_player/MusicPlayerApplication.java
+++ src/main/java/it/gruppo19/progetto_music_player/MusicPlayerApplication.java
@@ -1,5 +1,6 @@
 package it.gruppo19.progetto_music_player;
 
+import it.gruppo19.progetto_music_player.controller.RealMainController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -8,12 +9,21 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class MusicPlayerApplication extends Application {
+
+    private static Stage stage;
+
+    public static Stage GetStage(){return stage;}
+
     @Override
     public void start(Stage stage) throws IOException {
+        MusicPlayerApplication.stage = stage;
+        RealMainController controller = new RealMainController();
+        /*
         FXMLLoader fxmlLoader = new FXMLLoader(MusicPlayerApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 800);
         stage.setTitle("Unisafy");
         stage.setScene(scene);
+         */
         stage.show();
     }
 
--- src/main/java/it/gruppo19/progetto_music_player/view/DummyView.java
+++ src/main/java/it/gruppo19/progetto_music_player/view/DummyView.java
@@ -1,4 +1,20 @@
 package it.gruppo19.progetto_music_player.view;
 
+import it.gruppo19.progetto_music_player.MusicPlayerApplication;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class DummyView {
+
+    public DummyView() throws IOException {
+        Stage stage = MusicPlayerApplication.GetStage();
+        FXMLLoader fxmlLoader = new FXMLLoader(MusicPlayerApplication.class.getResource("main-view.fxml"));
+        Scene scene= new Scene(fxmlLoader.load(), 1280, 800);
+        stage.setTitle("Unisafy");
+        stage.setScene(scene);
+    }
+
 }
