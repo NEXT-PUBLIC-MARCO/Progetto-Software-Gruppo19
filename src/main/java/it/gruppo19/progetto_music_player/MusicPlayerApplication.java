10e8038bb9c9c1c715b7204d8b1480636ec88497
Carmine Mirante <c.mirante1@studenti.unisa.it>
2026-06-15 17:26:57 +0200

    Pulizia Entry point

--- src/main/java/it/gruppo19/progetto_music_player/MusicPlayerApplication.java
+++ src/main/java/it/gruppo19/progetto_music_player/MusicPlayerApplication.java
@@ -1,7 +1,11 @@
 package it.gruppo19.progetto_music_player;
 
-import it.gruppo19.progetto_music_player.controller.Controller;
+import it.gruppo19.progetto_music_player.controller.MainViewController;
+import it.gruppo19.progetto_music_player.model.DataModel;
+import it.gruppo19.progetto_music_player.storage.Storage;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -9,13 +13,27 @@ import java.io.IOException;
 public class MusicPlayerApplication extends Application {
 
     private static Stage stage;
-    public static Stage GetStage(){ return stage; }
+    private Storage storage;
+    private DataModel model;
+    private MainViewController controller;
+    //public static Stage GetStage(){ return stage; }
 
     @Override
     public void start(Stage stage) throws IOException {
         MusicPlayerApplication.stage = stage;
-        Controller controller = new Controller();
-        controller.OnAppStartup();
+        storage = new Storage();
+        model = new DataModel(storage.LoadBrani(), storage.LoadPlaylist());
+
+        FXMLLoader fxmlLoader = new FXMLLoader(MusicPlayerApplication.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 800);
+        controller = fxmlLoader.getController();
+
+        controller.setModel(model);
+        controller.setStorage(storage);
+        model.Attach(controller);
+        //controller.OnAppStartup();
+        stage.setTitle("Unisafy");
+        stage.setScene(scene);
         stage.show();
     }
 
