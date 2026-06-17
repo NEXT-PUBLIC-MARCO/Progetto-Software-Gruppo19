98632f899034595dbd77822518feb6b677f772b4
Carmine Mirante <c.mirante1@studenti.unisa.it>
2026-06-17 17:45:51 +0200

    Aggiunta LoopSingle

--- src/main/java/it/gruppo19/progetto_music_player/model/strategyPattern/PlaybackStrat.java
+++ src/main/java/it/gruppo19/progetto_music_player/model/strategyPattern/PlaybackStrat.java
@@ -6,4 +6,16 @@ import java.util.List;
 
 public interface PlaybackStrat {
     boolean hasNext(List<BranoModel> tracks, BranoModel current);
+
+    default BranoModel getNext(List<BranoModel> tracks, BranoModel current){
+        if(tracks != null && !tracks.isEmpty() && current != null)
+            current = tracks.get((tracks.indexOf(current) + 1) % tracks.size());
+        return current;
+    }
+
+    default BranoModel getPrevious(List<BranoModel> tracks, BranoModel current){
+        if(tracks != null && !tracks.isEmpty() && current != null)
+            current = tracks.get((tracks.indexOf(current) - 1 + tracks.size()) % tracks.size());
+        return current;
+    }
 }
